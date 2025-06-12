71f389ddfd2c730b5cea3fb8e068f68659d0d291
Jeff <jefferson.vivas.95@gmail.com>
2025-06-12 15:00:23 +0200

    feat: creacion de estructura de carpetas y conexion a la bbdd

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('./../');
+$dotenv->load();
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS']
+    );
+
+} catch (PDOException $e) {
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+}
--- public/index.php
+++ public/index.php
@@ -1,25 +1,15 @@
 <?php
-
+// Cargar el autoload de Composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('./../');
+use Dotenv\Dotenv;
+
+// Cargar variables de entorno
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-?>
+// Iniciar sesión
+session_start();
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/output.css">
-    <title>Document</title>
-</head>
-
-<body class="bg-red-500">
-    <header class="w-full h-28 bg-amber-300">header</header>
-
-</body>
-
-</html>
+// Configuración de rutas
+require_once __DIR__ . '/../router.php';
