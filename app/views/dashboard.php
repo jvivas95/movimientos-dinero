c0fa994e6bbeecc85a4377eddb54801c034bb7b5
Jeff <jefferson.vivas.95@gmail.com>
2025-06-29 01:39:25 +0200

    fix: error al mostrar que el error 404. feat: creacción del contenedor de los movimientos.

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -10,18 +10,17 @@ include_once __DIR__ . '/../../config.php';
     <link rel="stylesheet" href="<?php echo BASE_URL;?>/public/assets/css/style.css">
     <title>Login</title>
 </head>
-<body class="flex roboto bg-gray-600">
+<body class="flex roboto">
     <?php include_once (__DIR__ . '/layouts/sidebar.php') ?>
-    <main class="flex flex-col text-center bg-gray-600">
+    <main class="flex flex-col text-center w-full h-full bg-black">
         <div class="w-auto">
         <h1 class="border border-solid">DASHBOARD</h1>
         <div>
-            <ul>
-                <li>Titulo</li>
-            </ul>
+            <div id="contenedorMovimiento">
+                <div id="tituloMovimiento">Aqui va el titutlo</div>
+                <div id="importe">Aquí va el importe del movimiento</div>
+            </div>
         </div>
-        </div>
-        <a type="button" href="<?php echo APP_URL?>/views/registro.php">Registro</a>
     </main>
 </body>
 </html>
