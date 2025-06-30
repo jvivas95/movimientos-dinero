b38c52eb89a1790046d1762105c637b4e23cd830
Jeff <jefferson.vivas.95@gmail.com>
2025-06-30 14:52:55 +0200

    update: del controlador y modelo para movimientos

--- app/controller/MovimientoController.php
+++ app/controller/MovimientoController.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once(__DIR__ . '/../entities/Movimiento.php');
 require_once(__DIR__ . '/../model/movimientoModel.php');
 
 
@@ -18,7 +19,14 @@ class MovimientoController
         $importe = $_POST["importe"] ?? '';
         $observaciones = $_POST["observaciones"] ?? '';
 
-        $resultadoCrearMovimiento = movimientoModel::crearMovimiento($tituloMov, $importe, $observaciones);
+        $datos = new Movimiento(
+            null,
+            $tituloMov,
+            $importe,
+            $observaciones,
+            null,
+            null);
+        $resultadoCrearMovimiento = movimientoModel::crearMovimiento($datos);
 
         if ($resultadoCrearMovimiento) {
             header("Location: ?route=dashboard");
--- app/model/movimientoModel.php
+++ app/model/movimientoModel.php
@@ -1,12 +1,17 @@
 <?php
 
+require_once __DIR__ . '/../entities/Movimiento.php';
 require_once __DIR__ . '/../../db.php';
 
 class movimientoModel
 {
-    public static function crearMovimiento($tituloMov, $importe, $observaciones)
+    public static function crearMovimiento($datos)
     {
         global $pdo;
+
+        $tituloMov = $datos->getTitulo();
+        $importe = $datos->getImporte();
+        $observaciones = $datos->getObservaciones();
 
         $sql = "INSERT INTO movimiento (titulo, importe, observaciones) VALUES (?,?,?)";
         $stmt = $pdo->prepare($sql);
