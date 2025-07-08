c99015a58108525617cc0d4990474dad1ea84c3b
Jeff <jefferson.vivas.95@gmail.com>
2025-07-08 02:20:12 +0200

    update: dar estilo al dashboard

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -15,24 +15,17 @@ include_once __DIR__ . '/../../config.php';
 <body class="flex roboto">
     <?php include_once(__DIR__ . '/layouts/sidebar.php') ?>
     <div class="flex flex-col w-full">
-        <div>
-            <div>
-                <div>Usuario:</div>
-                <h1><?php echo $_COOKIE['nusuario'] ?></h1>
+        <?php include_once(__DIR__ . '/layouts/header.php') ?>
+        <div class="flex w-full justify-center items-center h-full">
+            <div class="flex flex-col justify-center items-center w-full border border-solid">
+                <?php foreach ($movimientos as $movimiento): ?>
+                    <div class="border border-solid">
+                        <div><?= $movimiento['titulo'] ?></div>
+                        <div><?= $movimiento['importe'] ?>€</div>
+                        <div><?= $movimiento['observaciones'] ?></div>
+                    </div>
+                <?php endforeach; ?>
             </div>
-            <div>
-                <div>ID:</div>
-                <div><?php echo $_COOKIE['id'] ?></div>
-            </div>
-        </div>
-        <div class="flex flex-col justify-center items-center h-v w-full border border-solid">
-            <?php foreach ($movimientos as $movimiento): ?>
-                <div class="border border-solid">
-                    <div><?= $movimiento['titulo'] ?></div>
-                    <div><?= $movimiento['importe'] ?>€</div>
-                    <div><?= $movimiento['observaciones'] ?></div>
-                </div>
-            <?php endforeach; ?>
         </div>
     </div>
 </body>
---
+++ app/views/layouts/header.php
@@ -0,0 +1,3 @@
+<div class="border border-solid h-20">
+    <h1>Hola, <?php echo $_COOKIE['nusuario'] ?></h1>
+</div>
