4630847df8ea2d9a72744ccd281089fbc4875aa4
Jeff <jefferson.vivas.95@gmail.com>
2025-07-08 15:00:36 +0200

    update: actualización del header

---
+++ README.txt
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./public/assets/css/tailwind.css -o ./public/assets/css/style.css --watch
--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,3 +1,8 @@
-<div class="border border-solid h-20">
-    <h1>Hola, <?php echo $_COOKIE['nusuario'] ?></h1>
+<div class="bg-purple-900 text-white flex justify-between h-auto p-3">
+    <div class="w-fit p-1">
+        <h1>Hola, <?php echo $_COOKIE['nusuario'] ?></h1>
+    </div>
+    <div class="border border-solid rounded-md bg-purple-400 p-1 w-fit ">
+        <button>Cerrar sesión</button>
+    </div>
 </div>
