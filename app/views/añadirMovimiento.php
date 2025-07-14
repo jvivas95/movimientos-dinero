3cbd4d68d61e3ba4ed40e8762f8c4d3f315e6b12
Jeff <jefferson.vivas.95@gmail.com>
2025-07-14 14:59:52 +0200

    update: cambios visuales en la vista de añadirMovimiento

--- app/views/añadirMovimiento.php
+++ app/views/añadirMovimiento.php
@@ -10,20 +10,20 @@ include_once __DIR__ . '/../../config.php';
     <link rel="stylesheet" href="<?php echo BASE_URL;?>/public/assets/css/style.css">
     <title>Añadir movimiento</title>
 </head>
-<body class="flex flex-col items-center justify-center h-screen">
+<body class="flex flex-col items-center justify-center h-screen bg-purple-900 text-white">
     <!--< ?php include_once (__DIR__ . '/layouts/sidebar.php') ?>-->
-    <main class="border border-solid">
+    <h1 class="text-center  text-5xl pb-6">AÑADIR MOVIMIENTO</h1>
+    <main class="">
         <div class="w-auto">
-        <h1 class="border border-solid text-center">AÑADIR MOVIMIENTO</h1>
         <div>
-            <form class="flex flex-col" action="?route=añadirMovimiento" method="POST">
-                <label>Título movimiento</label>
-                <input class="border border-solid" type="text" name="tituloMov" required>
+            <form class="flex flex-col text-center justify-center items-center" action="?route=añadirMovimiento" method="POST">
+                <label class="">Título movimiento</label>
+                <input class="border border-solid border-black rounded-2xl w-fit pl-2" type="text" name="tituloMov" required>
                 <label>Importe</label>
-                <input class="border border-solid" type="number" name="importe" required>
+                <input class="border border-solid border-black rounded-2xl pl-2" type="number" name="importe" required>
                 <label>Observaciones</label>
-                <textarea class="border border-solid" name="observaciones" rows="4" cols="50"></textarea>
-                <button class="border border-solid" type="submit">Añadir</button>
+                <textarea class="border border-solid border-black rounded-2xl p-2.5" name="observaciones" rows="4" cols="50"></textarea>
+                <button class="border border-solid border-black rounded-2xl" type="submit">Añadir</button>
             </form>
         </div>
         <a type="button" href="?route=dashboard">HOME</a>
