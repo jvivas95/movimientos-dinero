7b424adbf799407aab162abc6b0b4aa3d7e19571
Jeff <jefferson.vivas.95@gmail.com>
2025-07-15 15:00:25 +0200

    update: cambio de colores y se implanta una gamma de colores en común

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,8 +1,8 @@
-<div class=" text-white flex justify-between h-auto p-3">
+<div class="flex justify-between h-auto p-3">
     <div class="w-fit p-1">
         <h1>Hola, <?php echo $_COOKIE['nusuario'] ?></h1>
     </div>
-    <div class="border border-solid rounded-md bg-purple-400 p-1 w-fit transition-colors hover:bg-purple-500 duration-500">
+    <div class=" p-2 rounded-md bg-red-400 w-fit transition-colors hover:bg-red-500 duration-500">
         <a href="?route=añadirMovimiento">
             Cerrar sesión
         </a>
