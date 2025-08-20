23aa3440558b68cc3257d192d70e2c22592082a1
Jeff <jefferson.vivas.95@gmail.com>
2025-08-20 17:45:15 +0200

    update: sidebar

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -5,7 +5,7 @@
             <img src="<?php echo BASE_URL; ?>/public/assets/images/logo.png" alt="Logo" class="rounded-2xl w-full">
         </picture>
     </div>
-    <div class="border border-solid w-fit">
+    <div class="w-fit">
         <ul class="flex flex-col">
             <li class="flex items-center gap-2 p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-library-plus flex-shrink-0">
